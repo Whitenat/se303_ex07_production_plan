18da35e26b4dbc888d16b9aa1b41aefdd35224d2
Natashia White <whitenat@oregonstate.edu>
2021-05-16 16:00:42 -0700

    add no producers shortfall test

--- test/province_test.rb
+++ test/province_test.rb
@@ -23,5 +23,15 @@ class ProvinceTest < Minitest::Test
   	assert_equal(292, asia.profit)
   end
 
-  
+  def test_no_producers_shortfall
+  	data = {
+  		name: "No producers",
+  		producers: [],
+  		demand: 30,
+  		price: 20
+  	};
+  	noProducers = Province.new(data)
+  	assert_equal(30, noProducers.shortfall)
+  end
+
 end
