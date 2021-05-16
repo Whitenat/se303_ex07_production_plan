2e4a4e266505b0b0dda48e5233edb88b1c9fa072
Natashia White <whitenat@oregonstate.edu>
2021-05-16 15:54:00 -0700

    add province change production test

--- test/province_test.rb
+++ test/province_test.rb
@@ -16,5 +16,12 @@ class ProvinceTest < Minitest::Test
   	assert_equal(230, asia.profit)
   end
 
+  def test_province_change_production
+  	asia = Province.new(sample_province_data)
+  	asia.producers[0].production = 20
+  	assert_equal(-6, asia.shortfall)
+  	assert_equal(292, asia.profit)
+  end
+
   
 end
